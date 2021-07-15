c14ec85f6a814befdaef05f7aa31e54998439d79
John Oblenda <joandong2@gmail.com>
2021-07-14 23:32:26 -0400

    baseURL updated

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,11 +4,11 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "http://localhost:8000/api",
-    //baseURL: "https://libroh.herokuapp.com/api",
+    //baseURL: "http://localhost:8000/api",
+    baseURL: "https://libroh.herokuapp.com/api",
     withCredentials: true,
     headers: {
-      Authorization: token
-    }
+      Authorization: token,
+    },
   });
 };
